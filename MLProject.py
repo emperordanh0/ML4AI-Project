6fd770c9a6753a2cc142fbd583fe86e1d8e464c6
Danh_005 <51956280+emperordanh0@users.noreply.github.com>
2023-07-31 22:23:01 +0700

    Update MLProject.py

--- MLProject.py
+++ MLProject.py
@@ -7,34 +7,34 @@ from tensorflow.keras.models import load_model
 with zipfile.ZipFile("model.zip","r") as zip_ref:
     zip_ref.extractall("model")
 
-model = load_model('/model/model')
-def simple_preprocessing(text):
-    emoji_pattern = re.compile("["
-                               "\U0001F600-\U0001F64F"
-                               "\U0001F300-\U0001F5FF"
-                               "\U0001F680-\U0001F6FF"
-                               "\U0001F1E0-\U0001F1FF"
-                               "\U00002702-\U000027B0"
-                               "\U000024C2-\U0001F251"
-                               "]+", flags=re.UNICODE)
-    text = emoji_pattern.sub(r'', text)
-    text = re.sub(r'[^\w\s_]', '', text)
-    text = " ".join(text.split())
-    text = text.strip().lower()
-    return text
+# model = load_model('/model/model')
+# def simple_preprocessing(text):
+#     emoji_pattern = re.compile("["
+#                                "\U0001F600-\U0001F64F"
+#                                "\U0001F300-\U0001F5FF"
+#                                "\U0001F680-\U0001F6FF"
+#                                "\U0001F1E0-\U0001F1FF"
+#                                "\U00002702-\U000027B0"
+#                                "\U000024C2-\U0001F251"
+#                                "]+", flags=re.UNICODE)
+#     text = emoji_pattern.sub(r'', text)
+#     text = re.sub(r'[^\w\s_]', '', text)
+#     text = " ".join(text.split())
+#     text = text.strip().lower()
+#     return text
 
-st.title('Sentiment Analysis Project')
-x = st.text_input('Input text: ')
-x = simple_preprocessing(x)
-x = np.array([x])
-y = model.predict(x)
-y = y[0][0]
-if y <= 0.4:
-  sentiment = 'Tiêu cực'
-if y >= 0.6:
-  sentiment = 'Tích cực'
-else:
-  sentiment = 'Trung lập'
+# st.title('Sentiment Analysis Project')
+# x = st.text_input('Input text: ')
+# x = simple_preprocessing(x)
+# x = np.array([x])
+# y = model.predict(x)
+# y = y[0][0]
+# if y <= 0.4:
+#   sentiment = 'Tiêu cực'
+# if y >= 0.6:
+#   sentiment = 'Tích cực'
+# else:
+#   sentiment = 'Trung lập'
 
-if st.button('Xử lý'):
-  st.write('Hàm ý:', sentiment)
+# if st.button('Xử lý'):
+#   st.write('Hàm ý:', sentiment)
